159fad2f121f1475384ad7f4c68987e7c97794be
punithAJ-hub <punithaj0007@gmail.com>
2024-01-19 13:34:49 -0500

    Factorial of a number using recursion

---
+++ Algorithms/src/main/recursion/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+        // 3! = 3*2*1
+
+        System.out.println(factorial(3));
+    }
+
+    static int factorial(int n){
+        // break/base condition
+        if(n==1)
+            return 1;
+        return n * factorial(n-1);
+    }
+}
