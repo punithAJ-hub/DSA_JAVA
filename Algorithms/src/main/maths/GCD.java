66b321f09ac70e44e8efbbb2c6e8670a1f5882c3
punithAJ-hub <punithaj0007@gmail.com>
2024-01-27 11:36:48 -0500

    Maths operations LCM and GCD

--- Algorithms/src/main/maths/GCD.java
+++ Algorithms/src/main/maths/GCD.java
@@ -2,7 +2,14 @@ public class GCD {
     public static void main(String[] args) {
         int x = 44;
         int y = 132;
-        System.out.println(gcd(x,y));
+        int gcd = gcd(x,y);
+
+        System.out.println("HCF :  " + gcd(x,y));
+
+        int f=x/gcd;
+        int g = y/gcd;
+
+        System.out.println("LCM :" + (f*g*gcd));
     }
 
     static int gcd(int x , int y){
