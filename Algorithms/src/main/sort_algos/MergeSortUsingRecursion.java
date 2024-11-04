0b2db8bfeda5419d600295ae50628caa9e136799
punithAJ-hub <punithaj0007@gmail.com>
2024-11-04 08:42:45 -0500

    MergeSortUsingRecursion

--- Algorithms/src/main/sort_algos/MergeSortUsingRecursion.java
+++ Algorithms/src/main/sort_algos/MergeSortUsingRecursion.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class MergeSortUsingRecursion {
     public static void main(String[] args) {
-
         int[] arr = {5,4,2,1,3};
         System.out.println(Arrays.toString(merge(arr)));
     }
