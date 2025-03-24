6f9e6b5461baba113e973fb6bf07db22413b501d
punithAJ-hub <punithaj0007@gmail.com>
2025-03-24 16:17:33 -0400

    patterns using java

--- Algorithms/src/main/recursion/PatternsUsingRecursion/RevertedTriangle.java
+++ Algorithms/src/main/recursion/PatternsUsingRecursion/RevertedTriangle.java
@@ -4,6 +4,20 @@ public class RevertedTriangle {
 
     public static void main(String[] args) {
         revertedTriangle(4,0);
+        normalTriangle(4,0);
+    }
+
+    private static void normalTriangle(int row, int col) {
+        if(row<1)
+            return;
+        if(col<row){
+            normalTriangle(row,col+1);
+            System.out.print("* ");
+        }
+        else{
+            normalTriangle(row-1,0);
+            System.out.println();
+        }
     }
 
     private static void revertedTriangle(int row, int col) {
@@ -18,4 +32,6 @@ public class RevertedTriangle {
             revertedTriangle(row-1,0);
         }
     }
+
+
 }
